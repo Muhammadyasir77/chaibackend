681c97a198e04244262fa7ffb0f568b73c1e507e
Muhammad Yasir <dev.muhammadyasir@gmail.com>
2026-09-07 20:35:22 +0500

    updating Port variable

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 const express = require('express');
 
-const app = express()
-const port = 3000
+const app = express()   // express app created or browser server application created
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {       // registering a route 
   res.send('Hello World!')
 })
-app.get('/about', (req, res) => {
+app.get('/about', (req, res) => {  // registering a 2nd route
     res.send('<h2>About Page</h2>')
 })
 
-app.listen(process.env.port, () => {
-  console.log(`Example app listening on port ${process.env.port}`)
+app.listen(PORT, () => {     // starts the server and listens on the specified port
+  console.log(`Example app listening on port ${PORT}`)
 })
